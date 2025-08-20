e67846718c444a50dd5fd6321ae84d79f77b04f7
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-08-20 18:27:24 +0300

    added changes to transactions and fixed merge issue

--- api/transactions.ts
+++ api/transactions.ts
@@ -1,6 +1,10 @@
 import instance from ".";
 import { getToken } from "./storage";
 
+export type TransferPayload = {
+  amount: number;       // positive value
+  receiverId: number;   // recipient's numeric _id
+};
 
 {/*deposit */}
 export const depositMoney = async (amount: number) => {
@@ -15,11 +19,16 @@ export const withdrawMoney = async (amount: number) => {
   return res.data;
 };
 {/*Transfer */}
-export const transferMoney = async (amount: number) => {
+export const transferMoney = async (payload: TransferPayload) => {
   const token = await getToken();
-  const res = await instance.put("/mini-project/api/transactions/transfer/<username>", { amount }, { headers: { Authorization: `Bearer ${token}` } });
+
+  const res = await instance.put("/mini-project/api/transactions/transfer/<username>",
+    payload,
+    { headers: { Authorization: `Bearer ${token}` } }
+  );
   return res.data;
 };
+
 {/*get all transactions */}
 export const getAllTransactions = async () => {
   const data = await instance.get("/mini-project/api/transactions/my");
